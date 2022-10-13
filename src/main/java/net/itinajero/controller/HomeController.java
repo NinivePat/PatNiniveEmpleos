e84aad1e6cac27eb4f9fed9306a5c8f6b77a9a28
NinivePat <ninivepat@gmail.com>
2022-10-12 22:38:28 -0500

    Encriptamos los passwords, utilizamos algoritmos y utilerias videos 10, 11 y 12, carpeta 14

--- src/main/java/net/itinajero/controller/HomeController.java
+++ src/main/java/net/itinajero/controller/HomeController.java
@@ -12,13 +12,16 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import net.itinajero.model.Perfil;
@@ -40,6 +43,9 @@ public class HomeController {
 	@Autowired
     private IUsuariosService serviceUsuarios;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	
 	@GetMapping("/signup")
 	public String registrarse(Usuario usuario,Model model) {
@@ -48,7 +54,10 @@ public class HomeController {
 	
 	@PostMapping("/signup")
 	public String guardarRegistro(Usuario usuario, RedirectAttributes attributes) {
-		 //Ejercicio.
+		String pwdPlano = usuario.getPassword();
+		String pwdEncriptado = passwordEncoder.encode(pwdPlano);
+		usuario.setPassword(pwdEncriptado);
+		//Ejercicio.
 		 usuario.setEstatus(1);
 		 usuario.setFechaRegistro(new Date());
 		 
@@ -94,6 +103,12 @@ public class HomeController {
 		return "listado";
 	}
 	
+	@GetMapping("/bcrypt/{texto}")
+	@ResponseBody
+	public String encriptar(@PathVariable("texto") String texto) {
+		return texto + " Encriptado en Bcrypt: " + passwordEncoder.encode(texto);
+	}
+
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
 		//List<Vacante> lista = serviceVacantes.buscarTodas();s
@@ -132,6 +147,7 @@ public class HomeController {
 	    model.addAttribute("vacantes", lista);
 		return "home";
 	}
+	
 	/**
 	 * InitBinder para String si los detecta vacios en el Data Binding los settea a NULL 
 	 * @Param binder
